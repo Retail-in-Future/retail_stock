df38b5598d3d60712265927a957b17f44b468042
guozhao <zyybbgz@icloud.com>
2017-07-20 11:53:12 +0800

    mod maven to gradlegit status
    ;

--- src/main/java/com/thoughtworks/retailInFuture/stock/controller/StockContoller.java
+++ src/main/java/com/thoughtworks/retailInFuture/stock/controller/StockContoller.java
@@ -2,26 +2,18 @@ package com.thoughtworks.retailInFuture.stock.controller;
 
 import com.thoughtworks.retailInFuture.stock.bean.StockInfo;
 import com.thoughtworks.retailInFuture.stock.service.StockService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
-@RequestMapping("/products/{sku}/stock")
-
-@Api(value = "stock management", description = "The stock management of Retail in future")
-
 public class StockContoller {
 
     @Autowired
     private StockService stockService;
 
-    @ApiOperation(value = "View the sku stock", response = StockInfo.class)
-    @RequestMapping(method= RequestMethod.GET,produces = "application/json")
+    @RequestMapping(value = "/products/{sku}/stock", method = RequestMethod.GET)
     public StockInfo show(
             @ApiParam(value = "the product sku", required = true)
             @PathVariable("sku") String sku
@@ -29,8 +21,7 @@ public class StockContoller {
         return stockService.find(sku);
     }
 
-    @ApiOperation(value = "create stock", response = StockInfo.class)
-    @RequestMapping(method= RequestMethod.POST,produces = "application/json")
+    @RequestMapping(value = "/products/{sku}/stock", method = RequestMethod.POST)
     public StockInfo create(
             @ApiParam(value = "the product sku", required = true)
             @PathVariable("sku") String sku,
@@ -42,5 +33,9 @@ public class StockContoller {
         return stockService.save(stockInfo);
     }
 
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
     
 }
