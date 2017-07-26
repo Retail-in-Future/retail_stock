5f76b1aa9fd21e22c7746aceea04f64c843e5a5d
xyduan <xyduan@thoughtworks.com>
2017-07-26 09:19:00 +0800

    change the response data

--- src/main/java/com/thoughtworks/retailInFuture/stock/controller/StockController.java
+++ src/main/java/com/thoughtworks/retailInFuture/stock/controller/StockController.java
@@ -6,11 +6,15 @@ import com.thoughtworks.retailInFuture.stock.bean.view.StockInfo;
 import com.thoughtworks.retailInFuture.stock.service.StockService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static java.util.stream.Collectors.toList;
 
@@ -23,9 +27,14 @@ public class StockController {
 
     @ApiOperation(value = "View the stocks", response = List.class)
     @RequestMapping(method= RequestMethod.GET, produces = "application/json")
-    public List<StockInfo> list(){
-        return stockService.findAll().stream().map(r -> new ObjectMapper().convertValue(r, StockInfo.class)).collect(toList());
-
+    public ResponseEntity<Map<String, Object>> list(){
+        List<StockInfo> stockInfoList = stockService.findAll().
+                stream().map(r -> new ObjectMapper().convertValue(r, StockInfo.class))
+                .collect(toList());
+        Map<String, Object> objectMap = new HashMap<>();
+        objectMap.put("result", 1);
+        objectMap.put("data", stockInfoList);
+        return new ResponseEntity(objectMap, HttpStatus.OK);
     }
 
 }
